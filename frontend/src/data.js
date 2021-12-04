3a81c8ecee49a6b73a79973e1e053caefedcac4d
Amarachi <49257104+Marycynthia-mara@users.noreply.github.com>
2021-12-04 15:25:01 +0100

    created the frontend
    created the frontend using the create-react-app command that generates a basic React template to get started with

---
+++ frontend/src/data.js
@@ -0,0 +1,11 @@
+const data = {
+    products : [
+        {
+            _id : 1,
+            name : "Nike Slim Shirt",
+            category : "Shirts",
+            images: "../public/imagesp1.jpg",
+            
+        }
+    ]
+}
