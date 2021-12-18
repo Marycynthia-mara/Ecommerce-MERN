9be1e60d1fe06070d8b91af6b18e5a03329d2dcd
Amarachi <49257104+Marycynthia-mara@users.noreply.github.com>
2021-12-18 22:15:35 +0100

    created product and rating component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,137 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+
 function App() {
   return (
-    <div className="grid-container">
-    <header className="row">
-      <div>
-        <a className="brand" href="index.html">amazona</a>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="index.html">amazona</a>
+          </div>
+          <div>
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:id" component={ProductScreen}></Route>
+            <Route path="/" component={HomeScreen} exact></Route>
+          </Routes>
+        </main>
+        <footer className="row center">All right reserved</footer>
       </div>
-      <div>
-        <a href="cart.html">Cart</a>
-        <a href="signin.html">Sign In</a>
-      </div>
-    </header>
-    <main>
-      <div>
-        <div className="row center">
-          <div className="card">
-            <a href="product.html">
-              
-              <img className="medium" src="./images/p1.jpg" alt="product" />
-            </a>
-            <div className="card-body">
-              <a href="product.html">
-                <h2>Nike Slim Shirts</h2>
-              </a>
-              <div className="rating">
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-              </div>
-              <div className="price">$120</div>
-            </div>
-          </div>
-          <div className="card">
-            <a href="product.html">
-              
-              <img className="medium" src="./images/p1.jpg" alt="product" />
-            </a>
-            <div className="card-body">
-              <a href="product.html">
-                <h2>Nike Slim Shirts</h2>
-              </a>
-              <div className="rating">
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-              </div>
-              <div className="price">$120</div>
-            </div>
-          </div>
-          <div className="card">
-            <a href="product.html">
-              
-              <img className="medium" src="./images/p1.jpg" alt="product" />
-            </a>
-            <div className="card-body">
-              <a href="product.html">
-                <h2>Nike Slim Shirts</h2>
-              </a>
-              <div className="rating">
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-              </div>
-              <div className="price">$120</div>
-            </div>
-          </div>
-          <div className="card">
-            <a href="product.html">
-              
-              <img className="medium" src="./images/p1.jpg" alt="product" />
-            </a>
-            <div className="card-body">
-              <a href="product.html">
-                <h2>Nike Slim Shirts</h2>
-              </a>
-              <div className="rating">
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-              </div>
-              <div className="price">$120</div>
-            </div>
-          </div>
-          <div className="card">
-            <a href="product.html">
-              
-              <img className="medium" src="./images/p1.jpg" alt="product" />
-            </a>
-            <div className="card-body">
-              <a href="product.html">
-                <h2>Nike Slim Shirts</h2>
-              </a>
-              <div className="rating">
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-              </div>
-              <div className="price">$120</div>
-            </div>
-          </div>
-          <div className="card">
-            <a href="product.html">
-              
-              <img className="medium" src="./images/p1.jpg" alt="product" />
-            </a>
-            <div className="card-body">
-              <a href="product.html">
-                <h2>Nike Slim Shirts</h2>
-              </a>
-              <div className="rating">
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star"></i> </span>
-                <span> <i className="fa fa-star-half-o"></i> </span>
-              </div>
-              <div className="price">$120</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
-    <footer className="row center">All right reserved</footer>
-  </div>
+    </BrowserRouter>
   );
 }
 
