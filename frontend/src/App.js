26947add5250c486d531af79fa856dbdd9f77c67
Amarachi Ugwu <marycynthiamarachi@gmail.com>
2022-06-02 15:59:28 +0100

    added signin functionalities

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { signout } from "./actions/userActions";
 import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
+  const userSignin = useSelector((state) => state.userSignin);
+  const {userInfo} = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  }
 
   return (
     <BrowserRouter>
@@ -25,7 +33,19 @@ function App() {
                 <span className="badge">{cartItems.length}</span>
               )}
             </Link>
-            <Link to="/signin">Sign In</Link>
+            {
+              userInfo ? (
+                <div className="dropdown">
+                  <Link to="#">{userInfo.name} <i className="fa fa-caret-down">{' '}</i></Link>
+                  <ul className="dropdown-content">
+                    <Link to="#signout" onClick={signoutHandler}>Sign Out</Link>
+                  </ul>
+                </div>
+                
+              ) : (
+                <Link to="/signin">Sign In</Link>
+              )
+            }
           </div>
         </header>
         <main>
@@ -33,6 +53,7 @@ function App() {
             <Route path="/cart/:id" element={<CartScreen />}></Route>
             <Route path="/product/:id" element={<ProductScreen />}></Route>
             <Route path="/" element={<HomeScreen />} exact></Route>
+            <Route path="/signin" element={<SigninScreen />}></Route>
           </Routes>
         </main>
         <footer className="row center">All right reserved</footer>
